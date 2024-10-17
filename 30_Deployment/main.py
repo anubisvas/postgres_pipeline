284b3b06b3d49c541669561deda4602a9149a442
Pavel <anubisvas@gmail.com>
2024-10-18 01:17:55 +0300

    try to commit preliminary file

--- 30_Deployment/main.py
+++ 30_Deployment/main.py
@@ -1,69 +1,77 @@
-import joblib
 import pandas as pd
-
 from sklearn.pipeline import Pipeline
-from sklearn.impute import SimpleImputer
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.preprocessing import StandardScaler
 from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+
 from sklearn.model_selection import cross_val_score
+from sklearn.preprocessing import OneHotEncoder, FunctionTransformer
+from sklearn.preprocessing import StandardScaler
+
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
-from sklearn.neural_network import MLPClassifier
+from sklearn.svm import SVC
+from statsmodels.tools import categorical
+
+
+def filter_data(df):
+   columns_to_drop = [
+       'id',
+       'url',
+       'region',
+       'region_url',
+       'price',
+       'manufacturer',
+       'image_url',
+       'description',
+       'posting_date',
+       'lat',
+       'long'
+   ]
+
+   # Возвращаем копию датафрейма, inplace тут делать нельзя!
+   return df.drop(columns_to_drop, axis=1)
 
 
 def main():
-    print('Loan Prediction Pipeline')
+    df = pd.read_csv('data/homework.csv').drop('id', axis=1)
+    X = df.drop('price_category', axis=1)
+    y = df['price_category'].apply(lambda x: -1.0 if x=='low' else 0 if x=='medium' else 1)
 
-    df = pd.read_csv('data/loan_train.csv').drop('Loan_ID', axis=1)
+    prefilter = Pipeline(steps=[
+        ('filter', FunctionTransformer(filter_data))
+    ])
+    # Сохраним в переменную numerical имена всех числовых признаков нашего датасета
+    numerical_feats = X.select_dtypes(include=['int64', 'float64']).columns
 
-    X = df.drop('Loan_Status', axis=1)
-    y = df['Loan_Status'].apply(lambda x: 1.0 if x == 'Y' else 0.0)
+    # Сохраним в переменную categorical имена всех категориальных признаков нашего датасета
+    categorical_feats = X.select_dtypes(include=['object']).columns
 
-    numerical_features = X.select_dtypes(include=['int64', 'float64']).columns
-    categorical_features = X.select_dtypes(include=['object']).columns
+    # В категориальных фичах заменяем пропуски модой
+    categorical_transformer = Pipeline(steps = [
+    ('imputer', SimpleImputer (strategy='mode')),
+    ('encoder', OneHotEncoder (handle_unknown='ignore'))
+    ] )
 
-    numerical_transformer = Pipeline(steps=[
-        ('imputer', SimpleImputer(strategy='median')),
+    # В численных фичах заменяем пропуски медианой
+    numerical_transformer = Pipeline (steps =  [
+        ('imputer', SimpleImputer (strategy='median')),
         ('scaler', StandardScaler())
+    ] )
+
+    preprocessor = ColumnTransformer (transformers=[
+        ('numerical', numerical_transformer, numerical_feats),
+        ('categorical', categorical_transformer, categorical_feats)
     ])
+    print(preprocessor)
+    #print(f'pipeline')  # Press Ctrl+F8 to toggle the breakpoint.
+    #df_filtered = filter_data(df).copy()
+    #print(df_filtered.columns)
 
-    categorical_transformer = Pipeline(steps=[
-        ('imputer', SimpleImputer(strategy='most_frequent')),
-        ('encoder', OneHotEncoder(handle_unknown='ignore'))
-    ])
 
-    preprocessor = ColumnTransformer(transformers=[
-        ('numerical', numerical_transformer, numerical_features),
-        ('categorical', categorical_transformer, categorical_features)
-    ])
-
-    models = (
-        LogisticRegression(solver='liblinear'),
-        RandomForestClassifier(),
-        MLPClassifier(activation='logistic', hidden_layer_sizes=(256, 128, 64))
-    )
-
-    best_score = .0
-    best_pipe = None
-    for model in models:
-        pipe = Pipeline(steps=[
-            ('preprocessor', preprocessor),
-            ('classifier', model)
-        ])
-        score = cross_val_score(pipe, X, y, cv=4, scoring='accuracy')
-        print(f'model: {type(model).__name__}, acc_mean: {score.mean():.4f}, acc_std: {score.std():.4f}')
-
-        if score.mean() > best_score:
-            best_score = score.mean()
-            best_pipe = pipe
-
-    print(f'best model: {type(best_pipe.named_steps["classifier"]).__name__}, accuracy: {best_score:.4f}')
-    joblib.dump(best_pipe, 'loan_pipe.pkl')
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     main()
 
-
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
